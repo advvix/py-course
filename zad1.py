7b05c77afa1bac84ba597ab6ce2b328c15b614fa
advvix <48068613+advvix@users.noreply.github.com>
2019-02-28 16:02:41 +0100

    Update zad1.py

--- zad1.py
+++ zad1.py
@@ -55,3 +55,5 @@ show()
 
 #5 upload the solution as a Github repository. I suggest creating a directory for the whole python course and subdirectories lab1, lab2 etc. (0.5p)
 #Ad 5 Hint write in Google "how to create a github repo". There are plenty of tutorials explaining this matter.
+
+# https://github.com/advvix/py-course
