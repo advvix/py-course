976b11fc56c681620fe3e99ab4dfaf4e7a762f6d
advvix <48068613+advvix@users.noreply.github.com>
2019-03-06 16:34:36 +0100

    rdy

--- zad1.py
+++ zad1.py
@@ -18,22 +18,20 @@ def factorial(x):
 x=int(input("-"*20+"\nObliczanie silni\nWprowadz liczbe: "))
 print("Silnia: ",factorial(x))
 
+
 #3 write a function which takes an array of numbers as an input and finds the lowest value. Return the index of that element and its value (1p)
-list = [5,6,12,46,120,2,37,126,298,8383,13,14]
+list = [5,6,12,46,120,2,37,126,298,8383,13,333]
 #list = []
 #list = np.random.randint(1,280,12)
 
-k=2
-if list[0]>list[1]:
-    minl = list[1]
-else:
-    minl=list[0]
+min_value = list[0]
+for i in range(1,len(list)):
+    if min_value>list[i]:
+        min_value=list[i]
+        j = i
+print(list,'\nLowest value: ',min_value,'\nIndex: ',j)
 
-for j in range(10):
-    if minl>list[k]:
-        minl=list[k]
-    k+=1
-print(list,"\nLowest value: ",minl)
+
 
 #4 looking at lab1-input and lab1-plot files create your own python script that takes a number and returns any chart of a given length.
 #the length of a chart is the input to your script. The output is a plot (it doesn't matter if it's a y=x or y=e^x+2x or y=|x| function, use your imagination)
@@ -49,4 +47,4 @@ show()
 #5 upload the solution as a Github repository. I suggest creating a directory for the whole python course and subdirectories lab1, lab2 etc. (0.5p)
 #Ad 5 Hint write in Google "how to create a github repo". There are plenty of tutorials explaining this matter.
 
-# https://github.com/advvix/py-course
+#https://github.com/advvix/py-course
