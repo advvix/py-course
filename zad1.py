cf6aae77b7ef883090fef545132301b59bcad4e2
advvix <48068613+advvix@users.noreply.github.com>
2019-03-07 13:47:39 +0100

    Update zad1.py

--- zad1.py
+++ zad1.py
@@ -8,19 +8,24 @@ for i in range (56,101):
     print(y)
 
 #2 ask the user for a number and print its factorial (1p)
+x=-1
+while x<0 or x%1!=0:
+    x=float(input("\nObliczanie silni\nWprowadz liczbe >=0: "))
+
 def factorial(x):
     i=x-1
-    while i>0:
-        x=x*i
-        i=i-1
+    if x>0:
+        while i>0:
+            x=x*i
+            i=i-1
+    elif x==0:
+        x=1
     return x
 
-x=int(input("-"*20+"\nObliczanie silni\nWprowadz liczbe: "))
 print("Silnia: ",factorial(x))
 
-
 #3 write a function which takes an array of numbers as an input and finds the lowest value. Return the index of that element and its value (1p)
-list = [5,6,12,46,120,2,37,126,298,8383,13,333]
+list = np.array([5,6,12,46,120,2,5,126,298,8383,13,333])
 #list = []
 #list = np.random.randint(1,280,12)
 
@@ -29,9 +34,8 @@ for i in range(1,len(list)):
     if min_value>list[i]:
         min_value=list[i]
         j = i
-print(list,'\nLowest value: ',min_value,'\nIndex: ',j)
-
-
+min_index = np.where(list==min_value)[0]
+print(list,'\nLowest value: ',min_value,'\nIndex: ',min_index)
 
 #4 looking at lab1-input and lab1-plot files create your own python script that takes a number and returns any chart of a given length.
 #the length of a chart is the input to your script. The output is a plot (it doesn't matter if it's a y=x or y=e^x+2x or y=|x| function, use your imagination)
