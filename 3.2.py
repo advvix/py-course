8af1b4fbf4d6d60c451143ef78977bfd4a2a83a7
Advvx <242587@student.pwr.edu.pl>
2019-04-11 13:17:52 +0200

    update

--- 3.2.py
+++ 3.2.py
@@ -1,55 +1,51 @@
 import numpy as np
-import cs50
+#enum struktura
 #2 Write a function which takes sets of parameters of two figures and compares their fields. (4p)
 # The exemplary input is [[Circle, 4], [Rhombus, 2, 4]]
 # Expected output would be 'The first figure (Circle) has larger field'
-#3 Test your solutions
-
-def countField(type, x, y=None):
-    global field
-    x=int(x)
-    y=int(y)
-    #circle
-    if type =='kolo':
-        field = (x**2)*np.pi
-    #rectangle
-    if type =='prostkat':
-        field = x*y
-    #triangle or rhombus
-    if type =='trojkat' or type =='romb':
-        field = (1/2)*x*y
+dane = [['trojkat',2,4],['kolo',4,2]]
+win = True
+def countField(zestaw):
+    field = None
+    if zestaw[0] == 'kolo': #circle
+        field = (zestaw[1]**2)*np.pi
+    if zestaw[0] == 'prostkat': #rectangle
+        field = zestaw[1]*zestaw[2]
+    if zestaw[0] == 'trojkat' or zestaw[0] =='romb': #triangle or rhombus
+        field = (1/2)*zestaw[1]*zestaw[2]
     return field
 
-def compare(type1,type2,x1,x2,y1=1,y2=1):
-    fig1 = countField(type1,x1,y1)
-    fig2 = countField(type2,x2,y2)
-    global answ
-    if fig1>fig2:
-        answ = 'The first figure ('+type1+') has larger field'
-    elif fig1==fig2:
-        answ = "Both figures("+type1+","+type2+") have the same field"
-    elif fig1<fig2:
-        answ = "The second figure ("+type2+") has larger field"
+def compare(zestaw1,zestaw2):
+    fig1 = countField(zestaw1)
+    fig2 = countField(zestaw2)
+    answ = None
+    if fig1 > fig2:
+        answ = 'The first figure ('+zestaw1[0]+') has larger field'
+    elif fig1 == fig2:
+        answ = "Both figures("+zestaw1[0]+","+zestaw2[0]+") have the same field"
+    elif fig1 < fig2:
+        answ = "The second figure ("+zestaw2[0]+") has larger field"
     return print(answ)
 
-s,type,y=[True,None,0]
-dane = []
-while s==True:
-    type = input("Jaka figura? : ")
-    if type=='kolo' or type=='prostokat' or type=='trojkat' or type=='romb':
-        x = cs50.get_float("Wprowadz X")
-        if type != 'kolo':
-            y = cs50.get_float("Wprowadz Y")
-        if x>=0 and y>=0:
-            dane.append([type,x,y])
-            if len(dane) == 2:
-                compare(dane[0][0],dane[1][0],dane[0][1],dane[1][1],dane[0][2],dane[1][2])
-                s=False
-        else:
-            print("Zle dane")
-
-    if type!='kolo' and type!='prostokat' and type!='trojkat' and type!='romb':
-        print('Wprowadzono zla figure')
-
-
-#enum struktura
+try:
+    for i in [0,1]:
+        if len(dane[i]) == 0:
+            print('Wykryto pusta liste')
+        if dane[i][0] != 'kolo' and dane[i][0] != 'romb' and dane[i][0] != 'trojkat' and dane[i][0] != 'prostokat':
+            print('Wprowadzono zla nazwe figury lub zla kolejnosc wpisywania w zestawie',i+1)
+        for k in range(1,len(dane[i])):
+            if type(dane[i][k]) != int or type(dane[i][k]) != int:
+                print("Wymiary sa zle podane w zestawie",i+1)
+        if len(dane[i])<2:
+            print("Brak wymiarow w zestawie",i+1)
+        if (dane[i][0] == 'romb' or dane[i][0] == 'trojkat' or dane[i][0] == 'prostokat') and len(dane[i])==2:
+            print(dane[i][0],"potrzebuje 2 wymiarow.")
+        if dane[i][0] == 'kolo' and len(dane[i])==3:
+            print(dane[i][0],"nie potrzebuje 2 wymiarow.")
+    else:
+        compare(dane[0],dane[1])
+except:
+    if not dane:
+        print("Wykryto pusta liste")
+    if len(dane) == 1:
+        print('Wpisano tylko jeden zestaw')
